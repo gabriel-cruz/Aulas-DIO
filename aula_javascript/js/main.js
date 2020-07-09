6098d5ddd6d37c313241b31405026e0ab0950b6d
Gabriel Cruz <gabrielcruz7400@gmail.com>
2020-07-09 19:37:07 -0300

    Add files via upload

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,34 @@
+//var nome = "Gabriel Cruz";
+
+//alert("Bem vindo, " + nome);
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva"); //adiciona mais um elemento na lista
+
+//lista.pop("pêra") //remove um elemento da lista
+
+//lista.reverse()
+//console.log(lista);
+
+//var frutas = {nome: "maçã", cor: "vermelha"}
+//console.log(frutas);
+
+/*var idade = prompt("Confirme sua idade"); //pop-up com campo de texto
+
+if (idade >= 18){
+    alert("Acesso Permitido");
+}
+else{
+    alert("Acesso Negado");
+}*/
+
+//var data = new Date();
+//alert(data)
+
+function botao(){
+    document.getElementById("texto").innerHTML = "Obrigado por clicar";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
